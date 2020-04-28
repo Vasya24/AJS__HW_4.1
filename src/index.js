1020b0680a3d458b3de7556f9c43ffe6900ed6df
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-04-28 14:57:54 +0300

    Form added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let inp = document.querySelector(".input");
+let val = inp.value;
+val = Number.parseInt(val)
+
+try {
+    typeof(val) === "number";
+    console.log('Ага, ок')
+} catch {
+    console.error('Вообще не ок. Я принимаю только числа.')
+}
+
