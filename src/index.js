1e247f9f42aaf59064b2aa1222c9dcab2e787778
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-21 21:49:55 +0300

    I had no idea how to use try..catch structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 function numCheck(num) {
-    
-     num = parseInt(num, 10);
-
+    num = parseInt(num, 10);
+    if (num == 0) {
+      throw Error('Как?! Совсем ничего не нужно?')
+    };
+    let str = String(num);
+    if (str != num) {
+      throw Error('Кажется, что-то не так')
+    } else {
+      return num
+    }
     };
 
 
--- src/js/__tests__/index.test.js
+++ src/js/__tests__/index.test.js
@@ -1,11 +1,6 @@
 import { numCheck } from '../../index'
 
-test('Try/Catch checking', () => {
-  numCheck('0');
-  for (let num in arguments) {
-      num = parseInt(num, 10)
-  if (num === 0) {
-      throw ('Ничего не выйдет')
-  }
-}
+test('Checking', () => {
+  numCheck('120');
+  expect(typeof num === 'string').toBe(false)
 })
